92168573d9bfeb65479414628dd9bfe4ca6ff1db
Sakshat Bhattarai <bhattaraisakshat07@gmail.com>
2020-05-31 18:18:37 -0400

    print and write-to-file

--- fbmsg.py
+++ fbmsg.py
@@ -1,6 +1,34 @@
 import os
+import sys
 import json
 from datetime import datetime
+
+class Logger(object):
+    def __init__(self, filename):
+        self.terminal = sys.stdout
+        self.grpfile = open(filename, "w", encoding="utf-8")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.grpfile.write(message)  
+
+    def flush(self):
+        #flush method
+        pass
+
+
+def start_file(filename):
+    sys.stdout = Logger(filename)
+
+
+def stop_file():
+    sys.stdout.grpfile.close()
+    sys.stdout = sys.stdout.terminal
+
+
+def del_line():
+    sys.stdout.write("\033[K")
+
 
 def m_files(location):
     if location == "":
@@ -96,26 +124,37 @@ def count(msg):
     return total_count, details, words
 
 
-def p_words(total_count, unique_count, details, from_date, to_date):
+def p_words(g_name, total_count, details, words, from_date, to_date):
+    filename = g_name.replace(" ", "_") + "_analysis.txt"
+    start_file(filename)
+    print("Words Analysis for '" + g_name + "'")
     print("\nMessages from", from_date ,"to", to_date, "\n")
-    print("Total no. of words in the group:", total_count, "\n")
-    print("Total no. of unique words in the group:", unique_count, "\n")
+    print("Total no. of unique words in the group:", len(words), "\n")
+    print("Total no. of words in the group:", total_count, "\n")    
     for i in details:
         print(i, ":", details[i]['count'])
     print()
+    print("Overall top 50 words:")
+    print(*(i for i in stop(words)), sep=', ', end='')
+    parts = parts_count(details, words)
+    parts_sorted = words_parts(parts)
+    for i in parts_sorted:
+        print("\n")
+        print(i + ":")
+        print(*(parts_sorted[i][j][0] for j in range(50)), sep=', ', end='')
+    print()
+    print_word_count(words, filename)   
+    stop_file()
 
 
 def stop(words):
     sorted_words = sorted(words.items(), key=lambda word: word[1]['count'], reverse = True)
-    stop_words = []
     i = 0
     for s in sorted_words:
         i += 1
-        #print(s[0], ":", s[1]['count'])
-        stop_words.append(s[0])
         if i > 50:
             break
-    return stop_words
+        yield s[0]
 
 
 def parts_count(details, words):
@@ -137,8 +176,29 @@ def words_parts(parts):
     return parts_sorted
 
 
+def print_word_count(words, filename):
+    input_word = True
+    while input_word:
+        input_word = input("\n\nEnter the word to see the count:")
+        print()
+        if input_word == "":
+            break
+        try:
+            words_input = words[input_word]
+        except KeyError:
+            print("No such word in the message.")
+            del_line()
+        else:
+            del_line()
+            print("Word: ", input_word, "\n")
+            for i in words_input: 
+                print(i, ":", words_input[i])             
+    return
+            
+
+
 def main():
-    g_name = input("Enter the group Name:")
+    g_name = input("Enter the group name:")
     files = []
     while files == []:
         location = input("\nEnter the location of the message files \n(Just press 'Enter' if in the same folder):")
@@ -150,29 +210,12 @@ def main():
             for m in files:
                 print(m.strip(location + "/"))
             break
-    print("\nWords Analysis for", g_name)
+    print()
     msg = parse(files)
     from_date = msg['messages'][-1]['time'][:-7]
     to_date = msg['messages'][0]['time'][:-7]
     total_count, details, words = count(msg)
-    unique_count = len(words)
-    p_words(total_count, unique_count, details, from_date, to_date)
-    stop_words = stop(words)
-    print("Top 50 words: \n")
-    for i in stop_words:
-        print(i, end=", ")
-    parts = parts_count(details, words)
-    parts_sorted = words_parts(parts)
-    for i in parts_sorted:
-        print()
-        print(i)
-        for j in range(50):
-            print(parts_sorted[i][j][0], end=", ")
-    input_word = True
-    while input_word:
-        input_word = input("\nEnter the word to see the count:")
-        for i in words[input_word]: 
-            print(i, ":", words[input_word][i])
+    p_words(g_name, total_count, details, words, from_date, to_date)
     input("Enter to exit:")
     
     
